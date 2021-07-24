286411a5bb4155294e68d572fcf2813c281a03ab
Yuto Nishida <yuyu.nishida@gmail.com>
2021-07-23 18:45:13 -0700

    Add example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use pulldown_cmark::{Event, Options, Parser, html};
+
 fn main() {
-    println!("Hello, world!");
+    let md_input: &str = "~~cross~~";
+    let parser = Parser::new_ext(md_input, Options::all()).map(|event| match event {
+        Event::Text(text) => Event::Text(text.replace("~~", "tilde").into()),
+        _ => event,
+    });
+
+    let stdout = std::io::stdout();
+    let mut handle = stdout.lock();
+
+    html::write_html(&mut handle, parser).unwrap();
 }
